1d24c8d5a17485c09fe86b70d27c1e6576d5a0aa
Erika-Liliana <eri.purizaga92@gmail.com>
2016-04-10 17:13:59 -0500

    Mostrar spoiler, ocultar boton, usa hide y show

--- js/app.js
+++ js/app.js
@@ -12,3 +12,32 @@ function enviarAdvertencia()
 }
 
 $(document).ready(enviarAdvertencia);
+
+
+//Planear
+
+
+
+
+//ESCONDER SPOILER
+$(".spoiler span").hide();
+
+//2 AGREGAR BOTON
+$(".spoiler").append("<button> Mostrar Spoiler!</button>");
+
+//cuando se presiona el BOTON
+//agregar un handler al evento clic
+$(".spoiler button").click(function(){
+//mostrar  spoiler
+	$(".spoiler span").show();
+
+	//deshacer del boton
+	$(this).remove();
+})
+
+
+function enviarAdvertencia()
+{
+	jQuery(".spoiler").hide().show("slow");
+
+}
